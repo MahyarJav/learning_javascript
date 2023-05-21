d4bd9910ca8b1a10102f54910a4864689f151672
MahyarJav <6135125+MahyarJav@users.noreply.github.com>
2023-05-21 19:22:25 +1000

    Final upload of AirBnb clone

---
+++ react/project-two/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="../images/react-icon-small.png" alt="react logo" className="nav--logo" /> <div className="nav--logo_text"><p>ReactFacts</p></div>
+            <div className="nav--course"><p>React Course - Project 1</p></div>
+        </nav>
+    )
+}
